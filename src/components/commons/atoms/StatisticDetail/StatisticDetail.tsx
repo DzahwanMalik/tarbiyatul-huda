c42e08f8c4113a4abe11331289084209b2edb8ad
DzahwanMalik <malikdzahwan22@gmail.com>
2025-10-14 21:24:47 +0700

    fix: statistic title

--- src/components/commons/atoms/StatisticDetail/StatisticDetail.tsx
+++ src/components/commons/atoms/StatisticDetail/StatisticDetail.tsx
@@ -9,26 +9,31 @@ type Props = {
   target: number;
 };
 
-const StatisticDetail = forwardRef<HTMLHeadingElement, Props>((prop: Props, ref) => {
-  return (
-    <div className="grow text-center flex flex-col gap-7 items-center">
-      <div className="relative rounded-full">
-        <FontAwesomeIcon
-          icon={prop.icon}
-          className="absolute top-1/2 left-1/2 -translate-1/2 text-3xl text-[var(--color-gold)]"
-        />
+const StatisticDetail = forwardRef<HTMLHeadingElement, Props>(
+  (prop: Props, ref) => {
+    return (
+      <div className="grow text-center flex flex-col gap-7 items-center">
+        <div className="relative rounded-full">
+          <FontAwesomeIcon
+            icon={prop.icon}
+            className="absolute top-1/2 left-1/2 -translate-1/2 text-3xl text-[var(--color-gold)]"
+          />
+        </div>
+        <div className="text-center">
+          <div className="flex justify-center">
+            <h1
+              className="text-xl font-semibold text-[var(--color-text)]"
+              ref={ref}
+            >
+              0
+            </h1>
+            <span className="text-xl font-semibold text-[var(--color-text)]">{prop.title}</span>
+          </div>
+          <p className="text-xs text-[var(--color-text-muted)]">{prop.desc}</p>
+        </div>
       </div>
-      <div className="">
-        <h1
-          className="text-xl font-semibold text-[var(--color-text)]"
-          ref={ref}
-        >
-          0
-        </h1>
-        <p className="text-xs text-[var(--color-text-muted)]">{prop.desc}</p>
-      </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default StatisticDetail;

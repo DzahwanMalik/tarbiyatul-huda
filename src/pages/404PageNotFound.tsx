651d62ed7928c4acc31991c582bd3992fea2a201
DzahwanMalik <malikdzahwan22@gmail.com>
2025-10-14 15:34:10 +0700

    re-commit

---
+++ src/pages/404PageNotFound.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-3xl font-bold text-[var(--color-gold)]">404 - Page Not Found</h1>
+        </div>
+    );
+};
+
+export default PageNotFound;
